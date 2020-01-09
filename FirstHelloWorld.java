7eba511c55c62b1b20c5c8c01d91b0d69650bc95
Rudy Rajput <rudyrajput@rudys-imac.attlocal.net>
2020-01-08 21:46:25 -0600

    First Commit

---
+++ FirstHelloWorld.java
@@ -0,0 +1,17 @@
+package org.fxapps;
+
+        public class FirstHelloWorld{
+        String name = "Rudy";
+
+
+        public void setName(String name){
+
+        this.name = name;
+
+        }
+
+        public String getName(){
+
+        return this.name;
+        }
+        }
